ffc4354f80c7d37554581659d73498ee774e5195
Hojun Im <ghwnsdla8094@gmail.com>
2026-03-28 18:34:21 +0900

    initial commit

---
+++ src/test/java/lab/connor/aivault/AiVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.connor.aivault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
